fb9bce5abf834f83d521177da1712f9760ec34c0
rajnish <rajneeshkumar9674737@gmail.com>
2024-08-16 15:18:25 +0530

    bug:found

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -5,9 +5,10 @@ export async function GET(req: NextRequest) {
     try {
         const val = process.env.BASE_URL
         const test = process.env.TEST
+        const bro = process.env.BRO
         console.log(test)
         console.log(val)
-        return NextResponse.json({ success: true, message: "Token saved successfully",data:[val,test] }, { status: 200 });
+        return NextResponse.json({ success: true, message: "Token saved successfully",data:[val,test,bro] }, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json({ success: false, message: "error.message" }, { status: 500 });
