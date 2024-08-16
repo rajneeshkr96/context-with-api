f5148eab24fb230ccd231cfd4023959677f1fbe9
rajnish <rajneeshkumar9674737@gmail.com>
2024-08-16 14:33:32 +0530

    yml:test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,7 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     try {
         const val = process.env.BASE_URL
-        return NextResponse.json({ success: true, message: "Token saved successfully",data:val }, { status: 200 });
+        const test = process.env.TEST
+        return NextResponse.json({ success: true, message: "Token saved successfully",data:[val,test] }, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json({ success: false, message: "error.message" }, { status: 500 });
